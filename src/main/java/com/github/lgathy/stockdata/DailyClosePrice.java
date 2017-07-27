c449638dc3910ad126c6dda8daea394066620873
Lajos Gáthy <lajos.gathy@doctusoft.com>
2017-07-27 15:43:59 +0200

    Added expected smoketest results as asserts

--- src/main/java/com/github/lgathy/stockdata/DailyClosePrice.java
+++ src/main/java/com/github/lgathy/stockdata/DailyClosePrice.java
@@ -1,8 +1,13 @@
 package com.github.lgathy.stockdata;
 
+import lombok.Data;
+import lombok.experimental.Accessors;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+@Data
+@Accessors(chain = true)
 public class DailyClosePrice {
     
     private LocalDate date;
